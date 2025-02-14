96c451a9584e35ac19382286cd3790d4464ba289
katte <417560450@qq.com>
2025-02-14 20:55:38 +0800

    优化文件目录

---
+++ get_ntpclient_bin.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 定义仓库的所有者、仓库名称
+owner="katteXu"
+repo="ntpclient"
+# 指定保存路径
+save_path="./src-tauri/bin"
+
+# 创建保存路径（如果不存在）
+mkdir -p "$save_path"
+
+# 获取最新版本信息
+release_info=$(curl -s "https://api.github.com/repos/$owner/$repo/releases/latest")
+
+# 提取 assets 信息
+assets=$(echo "$release_info" | jq -c '.assets[]')
+
+echo $assets
+# 遍历 assets 并下载
+while read -r asset; do
+    asset_name=$(echo "$asset" | jq -r '.name')
+    asset_url=$(echo "$asset" | jq -r '.browser_download_url')
+    echo "Downloading $asset_name..."
+    curl -L -o "$save_path/$asset_name" "$asset_url"
+    if [ $? -eq 0 ]; then
+        echo "$asset_name downloaded successfully."
+    else
+        echo "Failed to download $asset_name."
+    fi
+done <<< "$assets"
--- scripts/show_sys.sh
+++
@@ -1,5 +0,0 @@
-# windows
-rustc -Vv | Select-String "host:" | ForEach-Object {$_.Line.split(" ")[1]}
-
-# Unix
-rustc -Vv | grep host | cut -f2 -d' '
